999010d0e06fe99c5e677c45014cbd33aff19f76
QC2168 <huangxinhong2125@gmail.com>
2022-03-25 18:51:24 +0800

    sort

--- src/sort/MergeSort.ts
+++ src/sort/MergeSort.ts
@@ -1,41 +1,55 @@
-let arr1 = [1, 5, 4, 5, 6, 7, 8, 2];
-function MergeSort(arr: number[], l: number, r: number) {
-  if (arr === null || arr.length < 2) {
-    return arr;
-  }
-  sort(arr1, 0, arr1.length - 1);
+// let arr1 = [1, 5, 4, 5, 6, 7, 8, 2];
+// function MergeSort(arr: number[], l: number, r: number) {
+//   if (arr === null || arr.length < 2) {
+//     return arr;
+//   }
+//   sort(arr1, 0, arr1.length - 1);
+// }
+
+// function sort(arr: number[], left: number, right: number) {
+//   if (left === right) {
+//     return
+//   }
+
+//   let mid = left + ((right - left) >> 1);
+//   sort(arr, left, mid);
+//   sort(arr, mid + 1, right);
+//   Merge(arr, left, mid, right);
+// }
+
+// function Merge(arr: number[], l: number, m: number, r: number) {
+//   let helpArr = [];
+//   let i=0
+//   let p1 = l;
+//   let p2 = m + 1;
+//   while (p1 <= m && p2 <= r) {
+//     helpArr[i++]=(arr[p1] < arr[p2] ? arr[p1++] : arr[p2++]);
+//   }
+//   //   当p1或者p2越界之后 （不可能同时越界）
+//   while (p1 <= m) {
+//     helpArr.push(arr[p1++]);
+//   }
+//   while (p2 <= r) {
+//     helpArr.push(arr[p2++]);
+//   }
+//   return helpArr
+// }
+
+
+// let aa =MergeSort(arr1,0,arr1.length-1)
+// console.log(aa);
+// export default void 0
+
+function merge<T=number>(leftArr:T,rightArr:T){
+  let help=[]
+  while()
+
+
 }
 
-function sort(arr: number[], left: number, right: number) {
-  if (left === right) {
-    return
-  }
-
-  let mid = left + ((right - left) >> 1);
-  sort(arr, left, mid);
-  sort(arr, mid + 1, right);
-  Merge(arr, left, mid, right);
+function mergeSort<T=number>(arr:T[]){
+  let mid:number = arr.length>>2
+  let leftArr=arr.slice(0,mid)
+  let rightArr=arr.slice(mid+1)
+  return merge(leftArr,rightArr)
 }
-
-function Merge(arr: number[], l: number, m: number, r: number) {
-  let helpArr = [];
-  let i=0
-  let p1 = l;
-  let p2 = m + 1;
-  while (p1 <= m && p2 <= r) {
-    helpArr[i++]=(arr[p1] < arr[p2] ? arr[p1++] : arr[p2++]);
-  }
-  //   当p1或者p2越界之后 （不可能同时越界）
-  while (p1 <= m) {
-    helpArr.push(arr[p1++]);
-  }
-  while (p2 <= r) {
-    helpArr.push(arr[p2++]);
-  }
-  return helpArr
-}
-
-
-let aa =MergeSort(arr1,0,arr1.length-1)
-console.log(aa);
-export default void 0
