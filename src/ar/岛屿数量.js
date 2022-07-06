6fc8b7325f2e1c38f47a7ac87b00fa12518f4e16
QC2168 <huangxinhong2125@gmail.com>
2022-07-06 18:22:54 +0800

    岛屿数量

---
+++ src/ar/岛屿数量.js
@@ -0,0 +1,25 @@
+function dfs(grid, r, c) {
+    // 判断 base case
+    // 如果坐标 (r, c) 超出了网格范围，直接返回
+    if (!inArea(grid, r, c)) {
+        return;
+    }
+        // 如果这个格子不是岛屿，直接返回
+        if (grid[r][c] != 1) {
+            return;
+        }
+        grid[r][c] = 2; // 将格子标记为「已遍历过」
+    
+
+    // 访问上、下、左、右四个相邻结点
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+}
+
+// 判断坐标 (r, c) 是否在网格中
+function inArea(grid, r, c) {
+    return 0 <= r && r < grid.length
+        && 0 <= c && c < grid[0].length;
+}
